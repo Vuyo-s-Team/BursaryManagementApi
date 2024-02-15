30a364d210128119d5c5393bbf7e1c2ac79893e6
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-15 19:48:32 +0200

    created script for creating the UniversityYearlyFundAllocation table

---
+++ BursaryManagement/Tables/UniversityYearlyFundAllocation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[UniversityYearlyFundAllocation]
+(
+  [UniversityFundID] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  [Budget] [money] DEFAULT 0,
+  [RemainingBudget] [money] DEFAULT 0,
+  [YearlyFundID] [int] DEFAULT YEAR(GETDATE()),
+  [UniversityID] [int] NOT NULL
+);
+GO
