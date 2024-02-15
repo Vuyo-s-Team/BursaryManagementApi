c528bfa434272c8193d007d9daf3b1b1c5eea055
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-02-15 20:30:58 +0200

    Updated student not have references for now

--- BursaryManagement/Tables/Student.sql
+++ BursaryManagement/Tables/Student.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[Student](
     [IDNumber] [varchar](13) NOT NULL,
     [BirthDate] [date] NOT NULL,
     [Age] [tinyint] NOT NULL,
-    [GenderID] [int] REFERENCES Gender(GenderID),
-    [UniversityID] [int] REFERENCES University(UniversityID),
-    [RaceID] [int] REFERENCES Race(RaceID)
+    [GenderID] [int] NOT NULL,
+    [UniversityID] [int] NOT NULL,
+    [RaceID] [int] NOT NULL
 )
