54e535ab1ecde7af86f19f5b381155f3ed38f417
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-15 16:51:10 +0200

    script for creating BBDYearlyFund table

---
+++ BursaryManagement/Tables/BBDYearlyFund.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[BBDYearlyFund]
+(
+  [YearlyFundID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+  [TotalAmount] MONEY DEFAULT 0,
+  [RemainingAmount] MONEY DEFAULT 0,
+  [FinancialYearStart] DATE DEFAULT GETDATE()
+);
+GO
