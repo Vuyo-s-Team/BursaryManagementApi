707a20b5bf78ba3963cd360669f9a3afc305facd
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-02-15 17:47:06 +0200

    Created document  table

---
+++ BursaryManagement/Tables/Documents.sql
@@ -0,0 +1,8 @@
+   CREATE TABLE Document(
+        [DocumentID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+        [Transcript] [varbinary](max) NULL,
+        [IdentityDocument] [varbinary](max) NULL,
+        [ApplicationID] [int],
+    );
+
+GO
