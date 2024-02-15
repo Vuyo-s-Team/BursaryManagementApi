ee643c93d0532403b226eb5d961fd1efd4115734
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-02-15 18:04:16 +0200

    Created hod  table

--- BursaryManagement/Tables/Documents.sql
+++ BursaryManagement/Tables/Documents.sql
@@ -1,4 +1,4 @@
-   CREATE TABLE Document(
+   CREATE TABLE [dbo].[Document](
         [DocumentID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
         [Transcript] [varbinary](max) NULL,
         [IdentityDocument] [varbinary](max) NULL,
---
+++ BursaryManagement/Tables/HeadOfDepartment.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[HeadOfDepartment](
+	[HeadOfDepartmentID] [int] PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+    [UserID] [int] NOT NULL,
+    [DepartmentID] [int] NOT NULL,
+	[UniversityID] [int] NOT NULL
+);
+GO
