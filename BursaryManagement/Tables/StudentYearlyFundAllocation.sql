1878b609e7a00f44db3d07e603c9b56c542c4681
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-02-15 20:44:37 +0200

    Updated the tables we will added references later

--- BursaryManagement/Tables/StudentYearlyFundAllocation.sql
+++ BursaryManagement/Tables/StudentYearlyFundAllocation.sql
@@ -2,8 +2,8 @@ CREATE TABLE [dbo].[StudentYearlyFundAllocation](
     [StudentFundID] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
     [Budget] [money] NOT NULL,
     [FinancialYearStart] [date] NOT NULL,
-    [UniversityFundID] [int] REFERENCES University(UniversityID),
-    [StudentFundID] [int] REFERENCES Student(StudentID)
+    [UniversityFundID] [int] NOT NULL,
+    [StudentFundID] [int] NOT NULL
 );
 
 GO
