cc4e889e53ce24d09beb26cbf89c35259797d581
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-16 10:12:53 +0200

    added a column

--- BursaryManagement/Tables/BBDYearlyFund.sql
+++ BursaryManagement/Tables/BBDYearlyFund.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[BBDYearlyFund]
   [YearlyFundID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
   [TotalAmount] MONEY DEFAULT 0,
   [RemainingAmount] MONEY DEFAULT 0,
-  [FinancialYearStart] DATE DEFAULT GETDATE()
+  [FinancialYearStart] DATE DEFAULT GETDATE(),
+  [StudentFundCap] Money DEFAULT 0
 );
 GO
