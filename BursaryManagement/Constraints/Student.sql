4a26c57b75ecf43eac8771ac92b273487b95cb85
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-02-16 11:45:12 +0200

    Updated student constraints

--- BursaryManagement/Constraints/Student.sql
+++ BursaryManagement/Constraints/Student.sql
@@ -3,14 +3,14 @@ ADD CONSTRAINT AgeCheck CHECK (Age <=  35);
 GO
 
 ALTER TABLE [dbo].[Student]
-ADD CONSTRAINT [ForeignKeyGenderID] FOREIGN KEY (GenderID) REFERENCES Gender(GenderID);
+ADD CONSTRAINT [ForeignKeyGenderID] FOREIGN KEY (GenderID) REFERENCES [dbo].[Gender](GenderID);
 GO
 
 ALTER TABLE [dbo].[Student]
-ADD CONSTRAINT [ForeignKeyRaceID] FOREIGN KEY (RaceID) REFERENCES Race(RaceID);
+ADD CONSTRAINT [ForeignKeyRaceID] FOREIGN KEY (RaceID) REFERENCES [dbo].[Race](RaceID);
 GO
 
 ALTER TABLE [dbo].[Student]
-ADD CONSTRAINT [ForeignKeyUniversityID] FOREIGN KEY (UniversityID) REFERENCES University(UniversityID);
+ADD CONSTRAINT [ForeignKeyUniversityID] FOREIGN KEY (UniversityID) REFERENCES [dbo].[University](UniversityID);
 GO
 
