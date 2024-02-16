5fa2ed6fc738c0373b026706ef3d9af05c1f5acb
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-16 10:15:32 +0200

    modified data types

--- BursaryManagement/Tables/BBDYearlyFund.sql
+++ BursaryManagement/Tables/BBDYearlyFund.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [dbo].[BBDYearlyFund]
 (
   [YearlyFundID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  [TotalAmount] MONEY DEFAULT 0,
-  [RemainingAmount] MONEY DEFAULT 0,
-  [FinancialYearStart] DATE DEFAULT GETDATE(),
-  [StudentFundCap] Money DEFAULT 0
+  [TotalAmount] [money] DEFAULT 0,
+  [RemainingAmount] [money] DEFAULT 0,
+  [FinancialYearStart] [date] DEFAULT GETDATE(),
+  [StudentFundCap] [money] DEFAULT 0
 );
 GO
