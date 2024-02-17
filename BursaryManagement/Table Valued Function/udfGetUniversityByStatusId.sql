af06b1f7aecf5a7152c31e9ee324b9fc652816d6
Ntobeko Malinga <ntobekothe2@gmail.com>
2024-02-17 10:30:28 +0200

    fix function to use inner join instead of left join

--- BursaryManagement/Table Valued Function/udfGetUniversityByStatusId.sql
+++ BursaryManagement/Table Valued Function/udfGetUniversityByStatusId.sql
@@ -2,6 +2,6 @@ CREATE FUNCTION [dbo].[udfGetUniversityByStatus](@StatusId int)
     RETURNS TABLE
 AS
     RETURN SELECT University.[UniversityID], University.[Name], University.[ProvinceID] FROM [dbo].[University] University 
-           LEFT JOIN [dbo].[UniversityFundApplication] UniversityFundApplication
+           INNER JOIN [dbo].[UniversityFundApplication] UniversityFundApplication
            ON University.UniversityID = UniversityFundApplication.UniversityID
 GO
