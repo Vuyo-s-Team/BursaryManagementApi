377fa567191915b50566660f155e57bafd934326
Madimetja Movundlela <Madimetja.Movundlela@bbd.co.za>
2024-02-20 01:24:06 +0200

    Consider race and gender

--- BursaryManagement/Stored Procedures/spCreateStudent.sql
+++ BursaryManagement/Stored Procedures/spCreateStudent.sql
@@ -2,7 +2,9 @@ CREATE PROCEDURE [dbo].[spCreateStudent](
     @FirstName varchar(120),
     @LastName varchar(120),
     @Email varchar(255),
-    @PhoneNumber varchar(255))
+    @PhoneNumber varchar(255),
+    @GenderID int,
+    @RaceID int)
 AS
 BEGIN
 BEGIN TRANSACTION;
