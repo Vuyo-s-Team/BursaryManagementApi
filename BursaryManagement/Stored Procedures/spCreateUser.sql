b0a254260df4c28640026225fd0c138cd18a1d7b
Madimetja Movundlela <Madimetja.Movundlela@bbd.co.za>
2024-02-19 16:51:32 +0200

    Create User

---
+++ BursaryManagement/Stored Procedures/spCreateUser.sql
@@ -0,0 +1,30 @@
+CREATE PROCEDURE [dbo].[spCreateUser](
+    @FirstName varchar(120),
+    @LastName varchar(120),
+    @Email varchar(255),
+    @PhoneNumber varchar(255)
+)
+AS
+BEGIN
+BEGIN TRANSACTION;
+BEGIN TRY
+
+    INSERT INTO [dbo].[ContactDetails](Email, PhoneNumber)
+    VALUES (@Email, @PhoneNumber);
+
+    DECLARE @ContactID int;
+    SET @ContactID = SCOPE_IDENTITY();
+
+    INSERT INTO [dbo].[User](FirstName, LastName, ContactID)
+    VALUES (@FirstName, @LastName, @ContactID);
+
+    DECLARE @UserID int;
+    SET @UserID = SCOPE_IDENTITY();
+
+    COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+    THROW;
+END CATCH;
+END;
